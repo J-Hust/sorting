0d3586112023f857eb3fba349252b5fc78333dc4
Minh Nguyen <minh@ellesa.com>
2018-04-23 11:43:03 -0500

    M added to bubblesort

--- bubblesort.js
+++ bubblesort.js
@@ -1,7 +1,22 @@
 
 function bubbleSort(arr){
-
-  return arr
+  let i=0, j = 0;
+  let localCompareCount=0, localSwap=0;
+  debugger;
+  // loop through using 1st element
+  console.log ('Before: ' + arr);
+  for (i=0; i<arr.length-1; i++){
+    for (j=0; j<arr.length-1-i; j++){
+      ++localCompareCount;
+      // each time through, increment the compareCount
+      if (arr[j] > arr[j+1]){
+        ++localSwap;
+        swap(arr, j, j+1);
+      }
+    }
+  }
+  console.log ('After: ' + arr);
+  return arr;
 }
 
 
